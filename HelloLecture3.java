6ea4b9305e460f6e56b3bdf1a88a47ee66aab509
SwarnaLalani <85065251+SwarnaLalani@users.noreply.github.com>
2021-06-01 23:02:13 +0530

    Add files via upload

---
+++ HelloLecture3.java
@@ -0,0 +1,6 @@
+class HelloLecture3{
+	public static void main(String[]args){
+		System.out.println("Welcome to java programming!");
+		System.out.print("This is the third lecture!");
+	}
+}
---
+++ MyNameisKate.java
@@ -0,0 +1,5 @@
+class MyNameisKate{
+public static void main(String[]args){
+	System.out.print("My name is Kate, I am in first semester of BIT degree program!");
+}
+}
