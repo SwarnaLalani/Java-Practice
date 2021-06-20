27a417d4f8b32551e30fa827693f1762198ae0c8
SwarnaLalani <85065251+SwarnaLalani@users.noreply.github.com>
2021-06-20 21:13:13 +0530

    First Commit

---
+++ IfGoToSchoolOrNot.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class IfGoToSchoolOrNot{
+	public static void main (String [] args){
+		Scanner a = new Scanner(System.in);
+		System.out.println("Enter 1, if it rains or Enter 0, if it does not");
+		int rain = a.nextInt();
+		if (rain==1){
+		System.out.println(" On days it rains, I do not go to school");
+		}else{
+		System.out.println(" On days it is sunny, I go to school");
+		}
+	}
+}
