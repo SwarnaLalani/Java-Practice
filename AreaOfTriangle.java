7be8d2160d157382ff23d3cb34b524c151d71df3
SwarnaLalani <85065251+SwarnaLalani@users.noreply.github.com>
2021-06-23 23:38:47 +0530

    2021.06.22 Evening

---
+++ AreaOfTriangle.java
@@ -0,0 +1,7 @@
+class AreaOfTriangle{
+public static void main(String[]args){
+	int b=12,h=6;area
+	area=(b*h)/2;
+System.out.println(area);	
+}
+}
---
+++ RainySunny.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class RainySunny{
+	
+	public static void main(String [] args){
+		Scanner x = new Scanner (System.in); // Create a new scanner
+		
+		System.out.println("Enter it is rainy");
+		String Weather = x.nextLine(); // Read user input
+		
+		if (Weather = it is rainy){
+		System.out.println("On days it rains, I do not go to school");	
+		}
+		System.out.println("Enter it is sunny");
+		else  (Weather = it is sunny){
+			
+		}
+		
+		System.out.println ("On days it is sunny")
+		
+		
+	}
+}
