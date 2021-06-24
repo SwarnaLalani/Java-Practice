7f03486194215662b12b391e2e96669b7e99ca91
SwarnaLalani <85065251+SwarnaLalani@users.noreply.github.com>
2021-06-24 13:31:27 +0530

    24th June 1

---
+++ Add43A79.java
@@ -0,0 +1,12 @@
+class Add43A79{
+	public static void main(String[]args){
+		
+		int x = 43; // Number of Students participated in the Lecture 1
+		
+		int y = 79; // Number of Students paticipated in the Lecture 2 
+		
+		int sum = x+y;
+		
+		System.out.println(sum);
+	}
+}
