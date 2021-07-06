93ae16edc7bfc840aa662ee5eb1d741803309556
SwarnaLalani <85065251+SwarnaLalani@users.noreply.github.com>
2021-07-06 22:16:10 +0530

    05th July 2021

---
+++ Factorial_1.java
@@ -0,0 +1,10 @@
+class Factorial_1{
+	public static void main(String [] args){
+		int fac = 1;
+		
+		for(int x=1;x<=10;x++){
+			fac = fac*x;
+			System.out.println(fac);
+		}
+	}
+}
---
+++ Sum_2_100N.java
@@ -0,0 +1,12 @@
+class Sum_2_100N{
+	public static void main(String [] args){
+		int i = 2;
+		int sum = 0;
+		while (i<=100){
+			sum = sum + i;
+			i=i+2;
+			
+		}
+		System.out.println(sum);
+	}
+}
