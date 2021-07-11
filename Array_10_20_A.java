7193897a50a1dcfe0332d04c08d65b7ad563c1aa
SwarnaLalani <85065251+SwarnaLalani@users.noreply.github.com>
2021-07-11 23:58:24 +0530

    11th July 2021

---
+++ Array_10_20_A.java
@@ -0,0 +1,13 @@
+class Array_10_20_A{
+	public static void main (String args []){
+		int sum = 10;
+		int []sum_array = new int [20];
+		for (int x = 10;x<20;x++){
+			sum = sum + x;
+			sum_array [x]=sum;
+		}
+		for (int y = 10; y < 20; y++){
+			System.out.println (sum_array [y]);
+		}
+	}
+}
---
+++ Even_50_60.java
@@ -0,0 +1,9 @@
+class Even_50_60{
+	public static void main (String args []){
+		int i = 50;
+		do{
+			System.out.println(i);
+			i=i+2;
+		}while (i<=60);
+	}
+}
